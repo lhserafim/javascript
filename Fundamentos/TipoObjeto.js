09002b8345f4fe0f8edb87e010cd05f2cb2217b9
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-09 09:25:23 -0300

    Tipo Objeto

---
+++ Fundamentos/TipoObjeto.js
@@ -0,0 +1,24 @@
+// Criando um objeto vazio
+const prod1 = {} ; // Par de chaves, significa que é um objeto
+
+// Adicionando DINAMICAMENTE atributos ao objeto
+// O JS aceita nome de atributos com espaço. NÃO USAR!
+prod1.nome = "Celular Ultra Mega Poderoso";
+prod1.preco = 49800.00;
+
+console.log(prod1); // { nome: 'Celular Ultra Mega Poderoso', preco: 49800 }
+
+const prod2 = {
+    nome: "Luiz Serafim", 
+    idade: 37,
+    filho: 
+        [{
+            nome: "Thiago",
+            idade: 2},
+        {
+            nome: "Alice",
+            idade: 1
+        }]
+};
+
+console.log(prod2);
