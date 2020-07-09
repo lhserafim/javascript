1edef3886b09bf0be483de6b2786515fbec51cdf
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-09 20:17:17 -0300

    Operadores de atribuiçao

---
+++ Fundamentos/OperadorAtribuicao.js
@@ -0,0 +1,12 @@
+// Operadores
+
+const a = 7;
+let b = 3;  // atribuição
+
+b += a; // operação aditiva
+b -= a; // operação subtrativa
+b *= a; // operação multiplicativa
+b /= a; // operação divisiva
+b %= a; // operação modular (pegar o resto)
+
+console.log(b);
