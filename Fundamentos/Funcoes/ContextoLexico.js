8165901b145b199014229dcde146c97452cc5abc
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-13 16:30:03 -0300

    Contexto léxico

---
+++ Fundamentos/Funcoes/ContextoLexico.js
@@ -0,0 +1,15 @@
+// Exemplo de contexto lexico 
+
+const local = 'Global';
+
+function minhaFunc() {
+    console.log(local);
+}
+
+function exec() {
+    const local = 'Local';
+    minhaFunc();
+}
+
+exec(); // Por causa do contexto lexico, a variável local que será usada é a declarada externamente 
+// A função carrega com sigo o lugar onde ela foi definida
