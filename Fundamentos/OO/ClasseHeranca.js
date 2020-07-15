803c94c672f0ae232ad8e1290420fde8e7483810
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-15 20:24:50 -0300

    Classes - 2 - Herança

---
+++ Fundamentos/OO/ClasseHeranca.js
@@ -0,0 +1,25 @@
+// Herança funciona em cima de prototype
+
+class Avo {
+    constructor(sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
+
+// Herança igual ao JAVA
+// Ou seja Pai tem um protótipo que é Avo
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'Professor') {
+        super(sobrenome); // Igual ao Java. O super() vai chamar a superclasse que aqui é o Avo
+        this.profissao = profissao;
+    }
+}
+
+class Filho extends Pai {
+    constructor() {
+        super('Silva'); // passei 1 parametro só para testar, poderia passar 2
+    }
+}
+
+const filho = new Filho();
+console.log(filho);
