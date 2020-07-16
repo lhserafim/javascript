597be832189370695425a4ba9970ef6be654e1fc
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-16 09:38:30 -0300

    Reduce

--- Fundamentos/Arrays/Reduce.js
+++ Fundamentos/Arrays/Reduce.js
@@ -19,3 +19,24 @@ const resultado = alunos.map(e => e.nota).reduce(function(acumulador, valorAtual
 }, 10); // o 10 é o valor inicial, usei aqui só para demonstrar. NÃO É OBRIGATÓRIO! E normalmente é 0 ou 1
 
 console.log('Resultado final:',resultado);
+
+// Todos os alunos são bolsistas?
+// Aplicar a lógica do && para retornar true apenas se condicao for verdadeira
+console.log(true && false); // false
+console.log(true && true); // true;
+// Então, sabendo da verdade acima, vou criar uma função arrow dentro de const todosBolsistas
+const todosBolsistas = (resp, bolsista) => resp && bolsista; // só retorna true se true && true
+console.log(alunos.map(a => a.bolsista)); // [ false, true, false, true ]
+// faço o .map para pegar o atributo bolsista de todos os elementos e passo p/ o reduce, que chama a funçao
+// que faz a operação lógica && 
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas)); // false
+// outra forma
+console.log(alunos.map(a => a.bolsista).reduce(function(acc, valor) {
+    return acc && valor;
+})); // false
+
+// Algum aluno é bolsista?
+console.log(alunos.map(a => a.bolsista).reduce(function(acc, valor) {
+    return acc || valor;
+})); // true
+// Também poderia substituir a function anonima por uma constante
