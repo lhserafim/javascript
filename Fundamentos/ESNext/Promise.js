31cc77993ecc9b614fa7f60b3bf8cabc291f2443
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-22 18:45:47 -0300

    Promise

--- Fundamentos/ESNext/Promise.js
+++ Fundamentos/ESNext/Promise.js
@@ -2,8 +2,37 @@
  * Promises nada mais é do que promessas em JS
  * Usamos quando fazemos um processamento assíncrono
  * Basicamente uma promessa pode ser Atendida (resolve) ou Não Atendida (reject)
- * Promises aceita só 1 parametro
+ * Promises aceita só 1 parametro, este pode ser um obj, array, string, etc 
+ * Promise é de algo que será executado no futuro e dará um retorno no futuro
  */
+
+console.log(typeof Promise); //function
+
+// Para eu usar uma Promise, eu preciso passar uma function para ela
+// Ex.:
+let p = new Promise(function(cumprirPromessa){
+    cumprirPromessa(['Ana', 'Luiz', 'Maria', 'João']);
+}) 
+
+// para eu executar a promessa, preciso passar .then(), QUE TAMBÉM TERÁ UMA FUNÇÃO passada a ela o valor
+// que foi cumprido
+p.then(function(valor){
+    console.log(valor);
+});
+
+p.then(valor => console.log(valor)); // versao arrow function
+
+// Encadeando .then
+const letraMinuscula = letra => letra.toLowerCase();
+p.then(valor => valor) // [ 'Ana', 'Luiz', 'Maria', 'João' ]
+ .then(valor => valor[0]) // Ana
+ .then(valor => valor[0]) // A
+ //.then(valor => valor.toLowerCase()) //a
+ .then(letraMinuscula)
+ .then(console.log)
+ //.then(valor => console.log(valor));
+
+console.log(typeof p); //object
 
 function falarDepoisDe(segundos, frase) {
     // O meu promise, está recebendo um callback do tipo arrow
