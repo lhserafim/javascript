beceafcd60d2c325e429b91a237e69254e095ce5
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-22 14:15:11 -0300

    SET

--- Fundamentos/ESNext/Set.js
+++ Fundamentos/ESNext/Set.js
@@ -1,3 +1,19 @@
 /**
- * O SET é uma estrutura não indexada e não aceita repetições
+ * O SET é uma estrutura de conjunto não indexada e não aceita repetições
  */
+
+const times = new Set(); // Inicializando, igual ao JAVA
+times.add('Vasco');
+times.add('Palmeiras').add('Curica').add('Bambi');
+times.add('Botafogo');
+times.add('Santos');
+
+console.log(times);
+console.log(times.has('vasco')); // false
+console.log(times.has('Vasco')); // true
+times.delete('Curica');
+console.log(times.has('Curica')); // false
+
+const escuderias = ['Ferrari', 'Mercedes', 'Red Bull', 'Alpha Tauri'];
+const escuderiasSet = new Set(escuderias);
+console.log(escuderias);
