0183403e3f7625f47ff40de31bfb1101abb93c28
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-25 08:32:35 -0300

    Formulários: 1. Criação da estrutura de pastas 2. Geração de package.json 3. Instalação das dependências 4. Criação da página de formulário 5. Parametrização do servidor

---
+++ Fundamentos/HTML/exercicios/formularios/server.js
@@ -0,0 +1,28 @@
+/**
+ * Vou acessar a pasta onde o server está /Users/lhserafim/Estudos/Java Script/Fundamentos/HTML/exercicios/formularios
+ * 1. Criar o arquivo package.json, via comando 'npm init -y'
+ * 2. Instalar dependências
+ * 2.1. npm i --save express@4.16.3 -E
+ * 2.2. npm i --save body-parser@1.18.2 -E
+ * 3. A pasta node_module e os arquivos package.json e package-lock.json serão criados
+ * 4. Configurar o servidor
+ * 5. IMPORTANTE para funcionar preciso definir a porta listener usando:
+ * app.listen(3003) 
+ * 6. Colocar a porta para escutar e digitar no terminal node.js
+ */
+
+
+// Vou requerer as dependencias
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express() // Carregar o express() em uma variável
+
+app.use(bodyParser.urlencoded({extended: true})) // e dizer que ela usa o bodyParser
+
+// Quero atender ao método POST em cima da URL /usuarios
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3003) // Colocar a porta para escutar e digitar no terminal node.js
