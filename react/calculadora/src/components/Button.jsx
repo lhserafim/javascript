c62a1511616e990a9fee30b664162a0f6b3a52c9
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-08-28 06:02:35 -0300

    370. Componente Button #01

---
+++ react/calculadora/src/components/Button.jsx
@@ -0,0 +1,9 @@
+// 370. Componente Button #01
+
+import React from 'react'
+import './Button.css' // Assim como no exemplo anterior, importo o css que é "par" do arquivo jsx
+
+// DICA
+// Sempre que possível, priorize o uso de componentes SEM ESTADO (STATE), pois este são mais faceis de manipular
+export default props =>
+<button className="button">{props.label}</button> // Recebe o parâmetro (props) label, do botão do calculator.jsx
--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -3,6 +3,8 @@
 import React, { Component } from 'react'
 import './Calculator.css' // O arquivo .css não precisa ser from alguma coisa
 
+import Button from '../components/Button' // 370. Componente Button #01
+
 // Utilizando a exportação padrão do ECMAscript 2015
 export default class Calculator extends Component {
     render() {
@@ -13,7 +15,24 @@ export default class Calculator extends Component {
             // 2. Pq apesar de ser um elemento HTML, este trecho de código é um JSX e não um HTML
             // 3. O react faz o transpile deste código para JS
             <div className="calculator"> {/*Esta className é referenciada em Calculator.css*/}
-
+                {/* 370. Componente Button #01 */}
+                <Button label="AC"/>
+                <Button label="/"/>
+                <Button label="7"/>
+                <Button label="8"/>
+                <Button label="9"/>
+                <Button label="*"/>
+                <Button label="4"/>
+                <Button label="5"/>
+                <Button label="6"/>
+                <Button label="-"/>
+                <Button label="1"/>
+                <Button label="2"/>
+                <Button label="3"/>
+                <Button label="+"/>
+                <Button label="0"/>
+                <Button label="."/>
+                <Button label="="/>
             </div>
         )
     }
