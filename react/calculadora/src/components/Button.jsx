b57f2ee50e4b68dde410e19bd2ea91885ad4eaaf
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-08-28 16:18:40 -0300

    372. Componente Button #02

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -6,4 +6,34 @@ import './Button.css' // Assim como no exemplo anterior, importo o css que é "p
 // DICA
 // Sempre que possível, priorize o uso de componentes SEM ESTADO (STATE), pois este são mais faceis de manipular
 export default props =>
-<button className="button">{props.label}</button> // Recebe o parâmetro (props) label, do botão do calculator.jsx
+//<button className="button">{props.label}</button> // Recebe o parâmetro (props) label, do botão do calculator.jsx
+
+// Aula 372. Componente Button #02
+// Aplicando a classe de forma dinâmica, de acordo com o que vem de parâmetro. Usando uma condição ternária, decido se aplico a classe
+// Dentro do template string, estou aplicando JS puro por isso estou usando jQuery
+<button 
+    onClick={e => props.click && props.click(props.label)} // Retornando o conteúdo do botão
+    className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}    
+`}>
+    {props.label}
+</button>
+
+// Versão 2 - é a mesma coisa da de cima, só escrito diferente
+// export default props => {
+//     let classes = 'button '
+//     classes += props.operation ? 'operation' : ''
+//     classes += props.double ? 'double' : ''
+//     classes += props.triple ? 'triple' : ''
+    
+//     return (
+//         <button 
+//             onClick={e => props.click && props.click(props.label)}
+//             className={classes}>
+//             {props.label}
+//         </button>
+//     )
+// }
--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -8,6 +8,29 @@ import Display from '../components/Display' // Aula 371. Componente Display
 
 // Utilizando a exportação padrão do ECMAscript 2015
 export default class Calculator extends Component {
+    // Aula 372. Componente Button #02
+
+    // Resolvendo o problema do this
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+    
+    // Criando as funções que vão serão chamadas de acordo com o botão clicado
+    clearMemory() {
+        console.log('limpar')
+    }
+
+    setOperation(operation) {
+        console.log(operation)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+
     render() {
         return (
             // IMPORTANTE!
@@ -19,23 +42,23 @@ export default class Calculator extends Component {
                 {/* Aula 371. Componente Display */}
                 <Display value={100}/>
                 {/* Aula 370. Componente Button #01 */}
-                <Button label="AC"/>
-                <Button label="/"/>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="*"/>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="6"/>
-                <Button label="-"/>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="+"/>
-                <Button label="0"/>
-                <Button label="."/>
-                <Button label="="/>
+                <Button label="AC" click={this.clearMemory} triple/>
+                <Button label="/" click={this.setOperation} operation/>
+                <Button label="7" click={this.addDigit}/>
+                <Button label="8" click={this.addDigit}/>
+                <Button label="9" click={this.addDigit}/>
+                <Button label="*" click={this.setOperation} operation/>
+                <Button label="4" click={this.addDigit}/>
+                <Button label="5" click={this.addDigit}/>
+                <Button label="6" click={this.addDigit}/>
+                <Button label="-" click={this.setOperation} operation/>
+                <Button label="1" click={this.addDigit}/>
+                <Button label="2" click={this.addDigit}/>
+                <Button label="3" click={this.addDigit}/>
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigit} double/>
+                <Button label="." click={this.addDigit}/>
+                <Button label="=" click={this.setOperation} operation/>
             </div>
         )
     }
