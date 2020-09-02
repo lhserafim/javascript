9f91a6c9a4fb14215f3c36d3f6cd4eb570ab3306
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-09-02 06:46:03 -0300

    382. Componente Navegação

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -4,5 +4,13 @@ import './Nav.css'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        {/* Aula 382. Componente Navegação */}
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
