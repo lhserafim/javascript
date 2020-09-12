3674d036e4723c2471c2eda4bf27445ae060299d
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-09-12 06:29:24 -0300

    407. Componente Display

--- vue/calculator/src/components/Display.vue
+++ vue/calculator/src/components/Display.vue
@@ -1,16 +1,29 @@
 // Aula 405. Criando Componentes
 <template>
     <div class="display">
-        0
+        {{ value }}
     </div>
 </template>
 
 <script>
 export default {
+    // Aula 407. Componente Display
+    props: ['value']
 
 }
 </script>
 
 <style>
+/* Aula 407. Componente Display */
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 20px;
+        font-size: 2.1em;
+        overflow: hidden;
+    }
 
 </style>
