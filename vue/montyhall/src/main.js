12962f2fce88d0a91f1cf4934048cbc5c9c3c8ab
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-09-13 05:45:41 -0300

    411. Problema de Monty Hall #01

---
+++ vue/montyhall/src/main.js
@@ -0,0 +1,13 @@
+// Aula 411. Problema de Monty Hall #01
+// Este é o arquivo entry point
+
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+    render: h => h(App)
+    // el: '#app',
+    // render(createElement) {
+    //     return createElement(App)
+    // }
+}).$mount("#app")
