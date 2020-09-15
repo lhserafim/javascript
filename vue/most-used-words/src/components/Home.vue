5046b5eb4a21e512cd7803a82792db8c48ed0da6
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-09-15 06:37:28 -0300

    418. Interface Gráfica com Vue JS #01

---
+++ vue/most-used-words/src/components/Home.vue
@@ -0,0 +1,32 @@
+// Aula 418. Interface Gráfica com Vue JS #01
+
+<template>
+    <v-container fluid>
+        <div class="pills">
+            <!-- Vou criar um for, para percorrer os elementos contidos no return do groupedWords 
+                Quando quero dizer ao vue para receber uma propriedade, utilizo o : (bind) -->
+            <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+export default {
+    components: { Pill },
+    data: function () {
+        return {
+            groupedWords: [
+                {name: 'i', amount: 1234},
+                {name: 'you', amount: 980},
+                {name: 'he', amount: 853}
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/most-used-words/src/components/Pill.vue
@@ -0,0 +1,19 @@
+Aula 418. Interface Gráfica com Vue JS #01
+
+<template>
+    <div>
+        <span>{{ name }}</span>
+        <span>{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    // Recebe as propriedades
+    props: ['name', 'amount']
+}
+</script>
+
+<style>
+
+</style>
