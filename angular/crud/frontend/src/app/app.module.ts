ef3fe5244e22c3ca69521d0302acd25084ef1852
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-09-24 06:50:56 -0300

    437. Componente Navegação

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -6,22 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-// Aula 435. Componente Cabeçalho
-// Importar o módulo que tem o toolbar
-import { MatToolbarModule} from '@angular/material/toolbar';
+// Aula 435. Componente Cabeçalho - Importar o módulo que tem o toolbar
+import { MatToolbarModule} from '@angular/material/toolbar'; 
+
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+// Aula 437. Componente Navegação - Importar o sidenav e list p/ usar no nav
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
