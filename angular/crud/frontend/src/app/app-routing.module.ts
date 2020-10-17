5d24036aba471447ad55a202e822596d4c3b0d7d
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-10-17 08:00:22 -0300

    Aula 459. Componente Atualizar Produto #01

--- angular/crud/frontend/src/app/app-routing.module.ts
+++ angular/crud/frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
@@ -18,7 +19,11 @@ const routes: Routes = [
   { // Aula 445. Componente Criar Produto
     path: "products/create",
     component: ProductCreateComponent
-  }
+  },
+  { // Aula 459. Componente Atualizar Produto #01
+    path: "products/update/:id", // passando um id como parametro
+    component: ProductUpdateComponent
+  }  
 ];
 
 @NgModule({
