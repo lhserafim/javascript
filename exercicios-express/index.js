c12ca9fc42d9385d38d676cff7f4e5e3c1f070b0
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-10-23 15:50:25 -0300

    514. Tratando a Primeira Requisição

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const app = express()
 
+// o use (ou o all) é utilizado p/ qualquer tipo de requisição (get, post, put, etc). Posso "travar" com app.get, etc.
+app.use((req, res) => {
+    res.send('Hello World!')
+})
+
 //app.listen(3000)
 // Vou colocar uma callback para mostrar que o listener funcionou.
 app.listen(3000, () => {
